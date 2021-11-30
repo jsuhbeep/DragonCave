77fa1aced8cb2883bd380096551b6766e9c62336
jsuhbeep <josephjsuh@brandeis.edu>
2021-11-30 00:22:30 -0500

    https://github.com/jsuhbeep/DragonCave.git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,13 +13,19 @@ public class Main {
         System.out.println("is greedy and hungry and will eat you on sight");
         System.out.println("Which cave will you go into? (1 or 2)");
         int choice = cave.nextInt();
-        System.out.println("You approach the cave... \nIt is dark and spooky...");
-        System.out.println("A large dragon jumps in front of you! He opens his jaws and...");
-        if(choice == 1) {
-            System.out.println("Gobbles you down in one bite!");
-        }else if(choice ==2){
-            System.out.println("says \"Hello\"! Welcome to my cave.");
-            System.out.println("He shares some of his treasure with you, and you leave safely.");
+        try {
+            int arr[] = {1,2};
+            int opt = arr[choice-1];
+            System.out.println("You approach the cave... \nIt is dark and spooky...");
+            System.out.println("A large dragon jumps in front of you! He opens his jaws and...");
+            if(choice == 1) {
+                System.out.println("Gobbles you down in one bite!");
+            }else if(choice ==2){
+                System.out.println("says \"Hello\"! Welcome to my cave.");
+                System.out.println("He shares some of his treasure with you, and you leave safely.");
+            }
+        } catch (Exception e){
+            System.out.println("Caught exception, input is neither 1 or 2.");
         }
     }
 }
